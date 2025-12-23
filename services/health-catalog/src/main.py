f14306aff45cea0230680bd006d525ce83d2c217
mahasa-amini <mahaasaamini@gmail.com>
2025-12-23 11:46:23 +0100

    get/topics added

--- services/health-catalog/src/main.py
+++ services/health-catalog/src/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from src.routers.thresholds import router as thresholds_router
+from src.routers.topics import router as topics_router
 
 app = FastAPI(title="Health Catalog Service")
 
 app.include_router(thresholds_router)
+app.include_router(topics_router)
 
