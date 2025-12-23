7c7f87240409f2a1c5fa851391b18ae698ca5967
mahasa-amini <mahaasaamini@gmail.com>
2025-12-23 12:24:34 +0100

    week 1 ended

--- services/health-catalog/src/main.py
+++ services/health-catalog/src/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 from src.routers.thresholds import router as thresholds_router
 from src.routers.topics import router as topics_router
 from src.routers.endpoints import router as endpoints_router
+from src.routers.patients import router as patients_router
+
 
 app = FastAPI(title="Health Catalog Service")
 
 app.include_router(thresholds_router)
 app.include_router(topics_router)
 app.include_router(endpoints_router)
+app.include_router(patients_router)
